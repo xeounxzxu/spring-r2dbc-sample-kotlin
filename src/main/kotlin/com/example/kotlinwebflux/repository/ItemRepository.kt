5ecfdbe6f9b7ce65bb37670adaaa4d7e8d78d14c
newbalancer <pnw123456@icloud.com>
2022-09-05 22:22:16 +0900

    feat: sample endpoint add

--- src/main/kotlin/com/example/kotlinwebflux/repository/ItemRepository.kt
+++ src/main/kotlin/com/example/kotlinwebflux/repository/ItemRepository.kt
@@ -3,7 +3,10 @@ package com.example.kotlinwebflux.repository
 import com.example.kotlinwebflux.domain.Item
 import org.springframework.data.r2dbc.repository.R2dbcRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
 
 @Repository
 interface ItemRepository : R2dbcRepository<Item, Long> {
+
+    
 }
