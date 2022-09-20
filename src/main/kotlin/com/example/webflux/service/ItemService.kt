1da64379959954e4024cd8468cbedd77724b96f8
newbalancer <pnw123456@icloud.com>
2022-09-20 21:40:38 +0900

    feat: change to coroutines

--- src/main/kotlin/com/example/webflux/service/ItemService.kt
+++ src/main/kotlin/com/example/webflux/service/ItemService.kt
@@ -2,8 +2,11 @@ package com.example.webflux.service
 
 import com.example.webflux.config.extend.MultiRouting
 import com.example.webflux.config.extend.MultiRoutingType
+import com.example.webflux.domain.Item
 import com.example.webflux.repository.ItemRepository
+import com.example.webflux.service.data.ItemDTO
 import com.example.webflux.service.data.ItemInfo
+import kotlinx.coroutines.flow.Flow
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -12,15 +15,15 @@ class ItemService constructor(
     private val itemRepository: ItemRepository,
 ) {
 
-    // @Transactional(value = "writeTransactionManager")
-    // fun created(dto: ItemDTO): Mono<Item> = itemRepository.save(dto.toCreatedStateEntity())
-    //
-    // @Transactional(value = "writeTransactionManager", readOnly = true)
-    // fun getAll(): Flux<Item> = itemRepository.findAll()
-    //
-    // @Transactional(value = "writeTransactionManager", readOnly = true)
-    // @Deprecated("not work method ... check point #1")
-    // fun get(id: Long): Mono<Item> = itemRepository.findById(id)
+    @Transactional(value = "writeTransactionManager")
+    suspend fun created(dto: ItemDTO): Item = itemRepository.save(dto.toCreatedStateEntity())
+
+    @Transactional(value = "writeTransactionManager", readOnly = true)
+    suspend fun getAll(): Flow<Item> = itemRepository.findAll()
+
+    @Transactional(value = "writeTransactionManager", readOnly = true)
+    @Deprecated("not work method ... check point #1")
+    suspend fun get(id: Long): Item? = itemRepository.findById(id)
 
     // todo: Multi DataSource Type Change AOP
     // this change to by datasource ...
--- src/main/kotlin/com/example/webflux/web/ItemController.kt
+++ src/main/kotlin/com/example/webflux/web/ItemController.kt
@@ -1,8 +1,14 @@
 package com.example.webflux.web
 
+import com.example.webflux.domain.Item
 import com.example.webflux.service.ItemService
+import com.example.webflux.service.data.ItemDTO
 import com.example.webflux.service.data.ItemInfo
+import kotlinx.coroutines.flow.Flow
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,14 +19,14 @@ constructor(
     private val itemService: ItemService,
 ) {
 
-    // @PostMapping("")
-    // fun created(@RequestBody dto: ItemDTO) = itemService.created(dto)
-    //
-    // @GetMapping()
-    // fun getAll(): Flux<Item> = itemService.getAll()
-    //
-    // @GetMapping("/{id}")
-    // fun get(@PathVariable id: Long): Mono<Item> = itemService.get(id)
+    @PostMapping("")
+    suspend fun created(@RequestBody dto: ItemDTO) = itemService.created(dto)
+
+    @GetMapping()
+    suspend fun getAll(): Flow<Item> = itemService.getAll()
+
+    @GetMapping("/{id}")
+    suspend fun get(@PathVariable id: Long): Item? = itemService.get(id)
 
     @GetMapping("/name")
     suspend fun get(name: String): ItemInfo? = itemService.get(name)
