2df8de1cc60cd7c7c4f76024e8e4184247903525
newbalancer <pnw123456@icloud.com>
2022-09-22 21:21:01 +0900

    test: ItemRepository Test add

--- data/src/main/kotlin/com/example/webflux/data/repository/ItemRepository.kt
+++ data/src/main/kotlin/com/example/webflux/data/repository/ItemRepository.kt
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository
 @Repository
 interface ItemRepository : CoroutineCrudRepository<Item, Long> {
 
-    fun findByName(name: String): Item
+    suspend fun findByName(name: String): Item?
 
     suspend fun <T> findByName(name: String, type: Class<T>): T?
 }
---
+++ data/src/test/resources/sql/item/item-data1.sql
@@ -0,0 +1,2 @@
+insert into item (name, type, count, created_at)
+values ('test', null, 0, 1663051887146);
