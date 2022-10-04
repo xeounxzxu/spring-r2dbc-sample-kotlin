1147d29695c5379243b8bc27e16041edece39f64
newbalancer <pnw123456@icloud.com>
2022-10-04 19:27:36 +0900

    test: product select query test case add

--- data/src/main/kotlin/com/example/webflux/repository/ProductRepository.kt
+++ data/src/main/kotlin/com/example/webflux/repository/ProductRepository.kt
@@ -1,9 +1,13 @@
 package com.example.webflux.repository
 
 import com.example.webflux.domain.Product
+import com.example.webflux.projection.ProductSalePointInfo
 import org.springframework.data.repository.kotlin.CoroutineSortingRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ProductRepository : CoroutineSortingRepository<Product, Long> {
+
+    // suspend fun <T> findProductById(id: Long, type: Class<T>): T?
+    suspend fun findProductById(id: Long): ProductSalePointInfo?
 }
--- data/src/test/kotlin/com/example/webflux/repository/ProductRepositoryTest.kt
+++ data/src/test/kotlin/com/example/webflux/repository/ProductRepositoryTest.kt
@@ -2,6 +2,7 @@ package com.example.webflux.repository
 
 import com.example.webflux.config.R2dbcTestConfiguration
 import com.example.webflux.domain.Product
+import com.example.webflux.projection.ProductSalePointInfo
 import com.example.webflux.util.MockUtil
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
@@ -40,5 +41,23 @@ internal class ProductRepositoryTest {
         assertEquals(mock.salePoint, entity.salePoint)
         assertNotNull(entity.createdAt)
         assertNotNull(entity.updatedAt)
+    }
+
+    @Test
+    @Order(2)
+    @OptIn(ExperimentalCoroutinesApi::class)
+    fun `상품 조회 테스트 케이스 1`() = runTest {
+
+        val mockId = 1L
+
+        val mock: Product =
+            MockUtil.readJsonFileToClass("json/product/save_.json", Product::class.java)!!
+
+        // todo : changed class file ...
+        val entity: ProductSalePointInfo = productRepository.findProductById(mockId)!!
+
+        assertNotNull(entity.getId())
+        assertEquals(mock.title, entity.getTitle())
+        assertEquals(mock.salePoint, entity.getSalePoint())
     }
 }
