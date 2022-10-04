c9c8fb1f2b98cbff6b7a916206c0fc83c9e2b33c
newbalancer <pnw123456@icloud.com>
2022-10-04 18:58:12 +0900

    feat: change kotlin styled

--- data/src/main/kotlin/com/example/webflux/callback/MileageCallBack.kt
+++ data/src/main/kotlin/com/example/webflux/callback/MileageCallBack.kt
@@ -1,13 +1,13 @@
 package com.example.webflux.callback
 
 import com.example.webflux.domain.Mileage
+import kotlinx.coroutines.reactor.mono
 import org.reactivestreams.Publisher
 import org.springframework.data.r2dbc.mapping.OutboundRow
 import org.springframework.data.r2dbc.mapping.event.AfterSaveCallback
 import org.springframework.data.r2dbc.mapping.event.BeforeSaveCallback
 import org.springframework.data.relational.core.sql.SqlIdentifier
 import org.springframework.stereotype.Component
-import reactor.core.publisher.Mono
 
 /**
  * Mileage Callback
@@ -18,16 +18,16 @@ class MileageCallBack : AfterSaveCallback<Mileage>, BeforeSaveCallback<Mileage> 
     /**
      * before method
      */
-    override fun onBeforeSave(entity: Mileage, row: OutboundRow, table: SqlIdentifier): Publisher<Mileage> {
-
-        return Mono.just(entity)
-    }
+    override fun onBeforeSave(entity: Mileage, row: OutboundRow, table: SqlIdentifier): Publisher<Mileage> =
+        mono {
+            entity
+        }
 
     /**
      * after method
      */
-    override fun onAfterSave(entity: Mileage, outboundRow: OutboundRow, table: SqlIdentifier): Publisher<Mileage> {
-
-        return Mono.just(entity)
-    }
+    override fun onAfterSave(entity: Mileage, outboundRow: OutboundRow, table: SqlIdentifier): Publisher<Mileage> =
+        mono {
+            entity
+        }
 }
