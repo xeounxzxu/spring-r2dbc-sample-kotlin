06e9a39658320dfc1d7d79e8d29ee1b890ab5f3d
newbalancer <pnw123456@icloud.com>
2022-10-29 10:30:06 +0900

    refactor: configuration beans is make up common

--- data/src/test/kotlin/com/example/webflux/config/QuerydslConfigurations.kt
+++ data/src/test/kotlin/com/example/webflux/config/QuerydslConfigurations.kt
@@ -3,23 +3,13 @@ package com.example.webflux.config
 import com.example.webflux.Main
 import com.querydsl.sql.MySQLTemplates
 import com.querydsl.sql.SQLTemplates
-import io.r2dbc.h2.H2ConnectionFactory
-import io.r2dbc.h2.H2ConnectionOption
-import io.r2dbc.spi.ConnectionFactory
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
-import org.springframework.core.io.ClassPathResource
-import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
-import org.springframework.r2dbc.connection.R2dbcTransactionManager
-import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator
-import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 import org.springframework.test.context.TestConstructor
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -28,38 +18,7 @@ import java.util.stream.Collectors
 import java.util.stream.Stream
 
 @TestConfiguration
-class QuerydslTestConfiguration : AbstractR2dbcConfiguration() {
-
-    @Bean(name = ["connectionFactory"])
-    override fun connectionFactory(): ConnectionFactory = H2ConnectionFactory.inMemory(
-        "testdb",
-        "sa",
-        "",
-        mapOf(
-            H2ConnectionOption.ACCESS_MODE_DATA to "MySQL"
-        )
-    )
-
-    @Bean(name = ["transactionManager"])
-    fun transactionManager(@Qualifier("connectionFactory") connectionFactory: ConnectionFactory) =
-        R2dbcTransactionManager(connectionFactory)
-
-    @Bean
-    fun initializer(@Qualifier("connectionFactory") connectionFactory: ConnectionFactory): ConnectionFactoryInitializer =
-        ConnectionFactoryInitializer().apply {
-
-            this.setConnectionFactory(connectionFactory)
-
-            val populator = CompositeDatabasePopulator().apply {
-                this.addPopulators(ResourceDatabasePopulator(ClassPathResource("sql/common/drop.sql")))
-                this.addPopulators(ResourceDatabasePopulator(ClassPathResource("sql/item/schema.sql")))
-                this.addPopulators(ResourceDatabasePopulator(ClassPathResource("sql/item/item-data1.sql")))
-                this.addPopulators(ResourceDatabasePopulator(ClassPathResource("sql/mileage/schema.sql")))
-                this.addPopulators(ResourceDatabasePopulator(ClassPathResource("sql/product/schema.sql")))
-            }
-
-            this.setDatabasePopulator(populator)
-        }
+class QuerydslTestConfiguration : AbstractR2dbRepositoryConfiguration() {
 
     @Bean
     fun sqlTemplates(): SQLTemplates {
--- data/src/test/kotlin/com/example/webflux/config/R2dbcTestConfiguration.kt
+++ data/src/test/kotlin/com/example/webflux/config/R2dbcTestConfiguration.kt
@@ -1,22 +1,10 @@
 package com.example.webflux.config
 
-import io.r2dbc.h2.H2ConnectionFactory
-import io.r2dbc.h2.H2ConnectionOption
-import io.r2dbc.spi.ConnectionFactory
-import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.boot.test.autoconfigure.data.r2dbc.AutoConfigureDataR2dbc
 import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.FilterType
-import org.springframework.core.io.ClassPathResource
-import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
-import org.springframework.r2dbc.connection.R2dbcTransactionManager
-import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator
-import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 
 @TestConfiguration
 @EnableR2dbcAuditing
@@ -32,36 +20,6 @@ import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
         "com.example.webflux.repository",
     ]
 )
-class R2dbcTestConfiguration : AbstractR2dbcConfiguration() {
+class R2dbcTestConfiguration : AbstractR2dbRepositoryConfiguration() {
 
-    @Bean(name = ["connectionFactory"])
-    override fun connectionFactory(): ConnectionFactory = H2ConnectionFactory.inMemory(
-        "testdb",
-        "sa",
-        "",
-        mapOf(
-            H2ConnectionOption.ACCESS_MODE_DATA to "MySQL"
-        )
-    )
-
-    @Bean(name = ["transactionManager"])
-    fun transactionManager(@Qualifier("connectionFactory") connectionFactory: ConnectionFactory) =
-        R2dbcTransactionManager(connectionFactory)
-
-    @Bean
-    fun initializer(@Qualifier("connectionFactory") connectionFactory: ConnectionFactory): ConnectionFactoryInitializer =
-        ConnectionFactoryInitializer().apply {
-
-            this.setConnectionFactory(connectionFactory)
-
-            val populator = CompositeDatabasePopulator().apply {
-                this.addPopulators(ResourceDatabasePopulator(ClassPathResource("sql/common/drop.sql")))
-                this.addPopulators(ResourceDatabasePopulator(ClassPathResource("sql/item/schema.sql")))
-                this.addPopulators(ResourceDatabasePopulator(ClassPathResource("sql/item/item-data1.sql")))
-                this.addPopulators(ResourceDatabasePopulator(ClassPathResource("sql/mileage/schema.sql")))
-                this.addPopulators(ResourceDatabasePopulator(ClassPathResource("sql/product/schema.sql")))
-            }
-
-            this.setDatabasePopulator(populator)
-        }
 }
