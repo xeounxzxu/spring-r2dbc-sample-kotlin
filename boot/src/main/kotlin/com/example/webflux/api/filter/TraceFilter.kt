3871bfc911bd97bcccafd924b536ad6257fc3ae1
newbalancer <pnw123456@icloud.com>
2022-11-04 23:03:20 +0900

    feat: TraceFilter add

---
+++ boot/src/main/kotlin/com/example/webflux/api/filter/TraceFilter.kt
@@ -0,0 +1,19 @@
+package com.example.webflux.api.filter
+
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
+import reactor.core.publisher.Mono
+
+@Component
+class TraceFilter : WebFilter {
+
+    val logger = LoggerFactory.getLogger(this::class.java)
+
+    override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
+        logger.info("filter start")
+        return chain.filter(exchange)
+    }
+}
