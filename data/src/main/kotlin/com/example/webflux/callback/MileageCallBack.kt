ac22951d38890b1e6b9f5c9505b411d54ade9b38
spark <pnw123456@icloud.com>
2024-01-04 22:02:53 +0900

    feat: update code

--- data/src/main/kotlin/com/example/webflux/callback/MileageCallBack.kt
+++ data/src/main/kotlin/com/example/webflux/callback/MileageCallBack.kt
@@ -6,6 +6,7 @@ import org.reactivestreams.Publisher
 import org.springframework.data.r2dbc.mapping.OutboundRow
 import org.springframework.data.r2dbc.mapping.event.AfterSaveCallback
 import org.springframework.data.r2dbc.mapping.event.BeforeSaveCallback
+import org.springframework.data.relational.core.mapping.event.AfterDeleteCallback
 import org.springframework.data.relational.core.sql.SqlIdentifier
 import org.springframework.stereotype.Component
 
@@ -13,7 +14,7 @@ import org.springframework.stereotype.Component
  * Mileage Callback
  */
 @Component
-class MileageCallBack : AfterSaveCallback<Mileage>, BeforeSaveCallback<Mileage> {
+class MileageCallBack : AfterSaveCallback<Mileage>, BeforeSaveCallback<Mileage>, AfterDeleteCallback<Mileage> {
 
     /**
      * before method
@@ -30,4 +31,8 @@ class MileageCallBack : AfterSaveCallback<Mileage>, BeforeSaveCallback<Mileage> 
         mono {
             entity
         }
+
+    override fun onAfterDelete(aggregate: Mileage): Mileage {
+        return aggregate
+    }
 }
