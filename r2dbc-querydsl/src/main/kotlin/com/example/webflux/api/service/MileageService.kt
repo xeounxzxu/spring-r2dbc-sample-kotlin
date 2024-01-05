ca875da9e992a123dbac5a4cf34eb28a44cc5408
spark <pnw123456@icloud.com>
2024-01-05 19:50:15 +0900

    feat: logger add

--- r2dbc-querydsl/src/main/kotlin/com/example/webflux/api/service/MileageService.kt
+++ r2dbc-querydsl/src/main/kotlin/com/example/webflux/api/service/MileageService.kt
@@ -5,6 +5,7 @@ import com.example.webflux.api.service.data.UpdateMileageDTO
 import com.example.webflux.domain.Mileage
 import com.example.webflux.domain.update
 import com.example.webflux.repository.MileageRepository
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -13,18 +14,23 @@ class MileageService(
     private val mileageRepository: MileageRepository,
 ) {
 
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     @Transactional(value = "reactiveTransactionManager")
     suspend fun created(dto: SaveMileageDTO): Mileage = mileageRepository.save(dto.toEntity())
 
     @Transactional(value = "reactiveTransactionManager")
-    suspend fun updated(userId: Long, dto: UpdateMileageDTO): Mileage =
-        checkNotNull(mileageRepository.findByUserId(userId))
-        .run {
+    suspend fun updated(userId: Long, dto: UpdateMileageDTO): Mileage {
+        logger.info("working ..")
+        return checkNotNull(mileageRepository.findByUserId(userId))
+            .run {
+                logger.info("working .. 2")
 
-            this.update(point = dto.point)
+                this.update(point = dto.point)
 
-            mileageRepository.save(
-                this
-            )
-        }
+                mileageRepository.save(
+                    this
+                )
+            }
+    }
 }
--- r2dbc-querydsl/src/main/kotlin/com/example/webflux/callback/MileageCallBack.kt
+++ r2dbc-querydsl/src/main/kotlin/com/example/webflux/callback/MileageCallBack.kt
@@ -5,6 +5,7 @@ import com.example.webflux.domain.MileageHistory2
 import com.example.webflux.repository.MileageHistoryRepository
 import kotlinx.coroutines.reactor.mono
 import org.reactivestreams.Publisher
+import org.slf4j.LoggerFactory
 import org.springframework.core.annotation.Order
 import org.springframework.data.r2dbc.mapping.OutboundRow
 import org.springframework.data.r2dbc.mapping.event.AfterSaveCallback
@@ -17,11 +18,15 @@ class MileageCallBack(
     private val mileageHistoryRepository: MileageHistoryRepository
 ) : AfterSaveCallback<Mileage> {
 
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     /**
      * after method
      */
     override fun onAfterSave(entity: Mileage, outboundRow: OutboundRow, table: SqlIdentifier): Publisher<Mileage> =
         mono {
+
+            logger.info("working callback")
 
             if (entity.point == 2000L) {
                 throw RuntimeException("fucking...!!")
